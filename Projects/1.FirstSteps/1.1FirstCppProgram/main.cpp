183c936774b02d31aa434b343cb3500fc5bc88b1
unknown <erskine.duenas@gmail.com>
2022-09-21 18:10:42 +0800

    update

--- Projects/1.FirstSteps/1.1FirstCppProgram/main.cpp
+++ Projects/1.FirstSteps/1.1FirstCppProgram/main.cpp
@@ -1,29 +1,31 @@
-#include <iostream>  //this is used to print input output
+#include <iostream>  //this is used to print input output ; this used to detect such as std:cout and std:endl
 
 int main(){   //main is the starting point of your c++ ; main is the very first thing that will run ; it is the entry point 
+    //std:cout << is used to print in C++
     std::cout << "Hello, World!" << std::endl;  // << std::endl; creates a new line
     return 0;
 }
 
 
-// how to run hello world?
+/* how to run hello world?
 
-    // Step 1: go to terminal tab 
-    // Step 2: click on "run task"
-    // Step 3: rooster.exe will be created/updated 
-    // Step 4: create new terminal
-    // Step 4: write >.\rooster.exe
+     Step 1: go to terminal tab 
+     Step 2: click on "run task"
+     Step 3: rooster.exe will be created/updated 
+     Step 4: create new terminal
+     Step 4: write >.\rooster.exe
 
 
 // to further elaborate std::endl; 
 
-    // std::cout << "Number1"; 
-    // std::cout << "Number2";
-    // *run*
-    // Number1Number2
+     std::cout << "Number1"; 
+     std::cout << "Number2";
+     *run*
+     Number1Number
+     std::cout << "Number1" << std::endl;
+     std::cout << "Number2";
+     *run*
+     Number1
+     Number2
 
-    // std::cout << "Number1" << std::endl;
-    // std::cout << "Number2";
-    // *run*
-    // Number1
-    // Number2
+*/
