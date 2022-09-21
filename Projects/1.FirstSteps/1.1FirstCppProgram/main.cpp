da69d3f6369b327eb58f52b89ef9dd732f1ea8dd
unknown <erskine.duenas@gmail.com>
2022-09-21 17:54:03 +0800

    update

--- Projects/1.FirstSteps/1.1FirstCppProgram/main.cpp
+++ Projects/1.FirstSteps/1.1FirstCppProgram/main.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
+#include <iostream>  //this is used to print input output
 
 consteval int get_value(){
     return 3;
 }
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    std::cout << "Hello, World!" << std::endl;  
     return 0;
 }
+
+
+// how to run hello world?
+
+    // Step 1: go to terminal tab 
+    // Step 2: click on "run task"
+    // Step 3: rooster.exe will be created/updated 
+    // Step 4: create new terminal
+    // Step 4: write >.\rooster.exe
