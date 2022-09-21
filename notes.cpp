e7ff80e1719ed27c1cd4ea6b47179f16f7dacf80
unknown <erskine.duenas@gmail.com>
2022-09-21 12:17:45 +0800

    first commit

---
+++ notes.cpp
@@ -0,0 +1,8 @@
+// how to setup C++ in vscode
+
+// Step 1: open Developer PowerShell for VScode 2022
+// Step 2: type >code . 
+// Step 3: *VS Code opens*
+// Step 4: New terminal
+// Step 5: Type in terminal >cl.exe  to check if no errors
+
