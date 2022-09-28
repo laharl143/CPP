ed1c35556a78bbe60bba4dcda256d09b319163ab
unknown <erskine.duenas@gmail.com>
2022-09-28 08:05:02 +0800

    update

--- Projects/1.FirstSteps/1.5DataInputAndOutput/main.cpp
+++ Projects/1.FirstSteps/1.5DataInputAndOutput/main.cpp
@@ -12,8 +12,10 @@ int main(){
     std::cerr << "Error message: Something is wrong!" << std::endl;
     std::clog << "Log message: Something happened" << std::endl;
 
-    */
-    //
+    
+    //Data Input
+
+    //create my variables
     int age1;
     std::string name;
 
@@ -22,6 +24,21 @@ int main(){
     std::cin >> age1;
 
     std::cout << "Hello " << name << "!, you are " << age1 << " years old." << std::endl;
+
+    */
+    //Data with spaces
+
+    //create my variables
+    std::string full_name;
+    int age3;
+
+    std::cout << "Please type your name and age: " << std::endl;
+
+    std::getline(std::cin,full_name);          //this line of code is used to input a data with spaces
+    // std::cin >> full_name;                  //this line of code doesn't work if your data has spaces
+    std::cin >> age3;
+
+    std::cout << "Hello " << full_name << "!, you are " << age3 << " years old." << std::endl;
 
     return 0;
 }
