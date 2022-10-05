885d474b21b5cf4fcfebb8557781a5f9d47d67e8
unknown <erskine.duenas@gmail.com>
2022-10-05 09:26:56 +0800

    update

--- Projects/2.VariablesAndDataTypes/2.8Auto/main.cpp
+++ Projects/2.VariablesAndDataTypes/2.8Auto/main.cpp
@@ -2,12 +2,12 @@
 
 
 int main(){
-/*
-	auto var1 {12};
-    auto var2 {13.0};
-    auto var3 {14.0f};
-    auto var4 {15.0l};
-    auto var5 {'e'};
+                        //auto is used to automatically assign a type (you can hover to a certain variable what type they are)
+	auto var1 {12};     //int
+    auto var2 {13.0};   //double
+    auto var3 {14.0f};  //flout
+    auto var4 {15.0l};  //long
+    auto var5 {'e'};    //char
     
     //int modifier suffixes
     auto var6 { 123u}; // unsigned
@@ -22,10 +22,17 @@ int main(){
     std::cout << "var6 occupies : " << sizeof(var6) << " bytes" << std::endl;
     std::cout << "var7 occupies : " << sizeof(var7) << " bytes" << std::endl;
     std::cout << "var8 occupies : " << sizeof(var8) << " bytes" << std::endl;
-*/
+/**/
     return 0;
 }
 /*
 final output: 
-
+var1 occupies : 4 bytes 
+var2 occupies : 8 bytes 
+var3 occupies : 4 bytes 
+var4 occupies : 16 bytes
+var5 occupies : 1 bytes 
+var6 occupies : 4 bytes 
+var7 occupies : 4 bytes 
+var8 occupies : 8 bytes 
 */
